4dcff31de3a1600ceb287310d9f5792c9458206d
Jeffrey Hu <hu000557@umn.edu>
2023-10-01 15:59:54 -0500

    Partial src/utils.c error checking, updated README

--- project-1/README.md
+++ project-1/README.md
@@ -1,15 +1,29 @@
 Project group number: 13
 
 Group member names and x500s:
-Jeffrey Hu (hu000557)
-Devajya Khanna (TODO)
-Sameen Rahman (rahma262)
+- Jeffrey Hu (hu000557)
+- Devajya Khanna (khann077)
+- Sameen Rahman (rahma262)
 
 The name of the CSELabs computer that you tested your code on: TODO
 
 Any changes you made to the Makefile or existing files that would affect grading: TODO
 
-Plan outlining individual contributions for each member of your group: TODO
+Plan outlining individual contributions for each member of your group:
+We plan to meet on Zoom and have one person share their screen while the 
+others assist by reviewing, looking up information in the writeup/documentation, etc.
+
+Every 30 minutes, we will switch the screen sharer/typer.
+
+Generally, the above process will result in the following assignments.
+- README: Jeffrey Hu, Sameen Rahman
+- 3.1 Partitioning Input Data into N files: Jeffrey Hu, Devajya Khanna, Sameen Rahman
+- 3.2 Creating Merkle Process Tree and Hashing Files: Switch every 30 minutes
+    - 3.2.1 Leaf Process: Switch every 30 minutes
+    - 3.2.2 Non-Leaf Process: Switch every 30 minutes
+- merkle.c: Switch every 30 minutes
+- Error Handling: Switch every 30 minutes
+- Code Review: Jeffrey Hu, Devajya Khanna, Sameen Rahman
 
 Plan on how you are going to implement the process tree component of creating the Merkle tree (high-level pseudocode would be acceptable/preferred for this part):
 main:
--- project-1/src/utils.c
+++ project-1/src/utils.c
@@ -5,6 +5,11 @@
 void partition_file_data(char *input_file, int n, char *blocks_folder) {
     // Hint: Use fseek() and ftell() to determine the size of the file
     FILE *fp = fopen(input_file, "r");
+    if (fp == NULL) { // error checking
+        perror("Failed to open input file.\n");
+        exit(-1);
+    }
+
     fseek(fp, 0, SEEK_END);
     long int input_file_size = ftell(fp);
 
@@ -15,27 +20,42 @@ void partition_file_data(char *input_file, int n, char *blocks_folder) {
     fseek(fp, 0, SEEK_SET);
 
     for(int i=0; i<n; i++){
-	//open new file & follow naming convention
-	char fout_name[50];
-	sprintf(fout_name, "%s/%d.txt", blocks_folder, i);
+        //open new file & follow naming convention
+        char fout_name[PATH_MAX];
+        sprintf(fout_name, "%s/%d.txt", blocks_folder, i);
 
-	//read input file
-	int fout_size = 0;
-	if(i<n-1){fout_size = output_file_size;}
-	else{fout_size = last_out_file_size;}
+        //read input file
+        int fout_size = 0;
+        if(i<n-1){fout_size = output_file_size;}
+        else{fout_size = last_out_file_size;}
 
-	char fout_contents[fout_size];
+        char fout_contents[fout_size];
+        if (fread(fout_contents, 1, fout_size, fp) < fout_size) { // error checking
+            perror("Failed to read entire block from input file.\n");
+            
+            // TODO: close files
+            
+            exit(-1);
+        }
+        // TODO: delete
+        // for (size_t blocks_read = fread(fout_contents, 1, fout_size, fp); blocks_read < fout_size; blocks_read = fread(fout_contents, 1, fout_size - blocks_read, fp)) { // error checking
+        //     perror("Failed to read entire block from input file.\n");
+        //     exit(-1);
+        // }
 
-	//needs some error checking
-	fread(fout_contents, 1, fout_size, fp);
+        //write these contents into newly opened file
+        FILE *foutp = fopen(fout_name, "w");
+        if (foutp == NULL) { // error checking
+            perror("Failed to open output file.\n");
+            exit(-1);
+        }
 
-	//write these contents into newly opened file
-	FILE *foutp = fopen(fout_name, "w");
+        if (fwrite(fout_contents, 1, fout_size, foutp) < fout_size) { // error checking
+            perror("Failed to write entire block to output file.\n");
+            exit(-1);
+        }
 
-	//needs some error checking
-	fwrite(fout_contents, 1, fout_size, foutp);
-
-	fclose(foutp);
+        fclose(foutp);
     }
 
     fclose(fp);
