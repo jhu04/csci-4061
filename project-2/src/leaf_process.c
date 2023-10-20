db3f9f599f2277756da5bbd9a712b179795dcecc
Jeffrey Hu <hu000557@umn.edu>
2023-10-19 21:39:44 -0500

    Finished intermediate submission

--- project-2/src/leaf_process.c
+++ project-2/src/leaf_process.c
@@ -7,6 +7,8 @@
 
 #define PATH_MAX 1024
 
+#define SHA256_STRING_SIZE (2 * SHA256_BLOCK_SIZE + 1)
+
 char *output_file_folder = "output/inter_submission/";
 
 int main(int argc, char* argv[]) {
@@ -20,12 +22,12 @@ int main(int argc, char* argv[]) {
     int pipe_write_end = atoi(argv[2]);
 
     //TODO(): create the hash of given file
-    char hash[2*SHA256_BLOCK_SIZE+1];
-    memset(hash, '\0', 2*SHA256_BLOCK_SIZE+1);
+    char hash[SHA256_STRING_SIZE];
+    memset(hash, '\0', SHA256_STRING_SIZE);
     hash_data_block(hash, file_path);
 
-    //TODO(): construct string write to pipe. The format is "<file_path>|<hash_value>"
-    char to_write[PATH_MAX];
+    //TODO(): construct string write to pipe. The format is "<file_path>|<hash_value>|"
+    char to_write[PATH_MAX + SHA256_STRING_SIZE + 2]; // TODO: increase buffer size
     memset(to_write, '\0', PATH_MAX);
     strcpy(to_write, file_path);
     strcat(to_write, "|");
@@ -36,27 +38,32 @@ int main(int argc, char* argv[]) {
         //TODO(inter submission)
         //TODO(overview): create a file in output_file_folder("output/inter_submission/root*") and write the constructed string to the file
         //TODO(step1): extract the file_name from file_path using extract_filename() in utils.c
-	char *file_name = extract_filename(file_path);
-	//printf("Filename: %s\n", file_name);
+        char *file_name = extract_filename(file_path);
+        //printf("Filename: %s\n", file_name);
 
         //TODO(step2): extract the root directory(e.g. root1 or root2 or root3) from file_path using extract_root_directory() in utils.c
-	char *root_dir = extract_root_directory(file_path);
-	//printf("Rootdir: %s\n", root_dir);
+        char *root_dir = extract_root_directory(file_path);
+        //printf("Rootdir: %s\n", root_dir);
 
         //TODO(step3): get the location of the new file (e.g. "output/inter_submission/root1" or "output/inter_submission/root2" or "output/inter_submission/root3")
-	char location[PATH_MAX];
-	strcpy(location, output_file_folder);
-	strcat(location, root_dir);
-	strcat(location, file_name);
+        char location[PATH_MAX];
+        memset(location, '\0', PATH_MAX);
+        strcpy(location, output_file_folder);
+        strcat(location, root_dir);
+        strcat(location, file_name);
 
-	printf("Loc: %s\n", location);
         //TODO(step4): create and write to file, and then close file
-	FILE *fp = fopen("test.txt", "w+");
-	fwrite(to_write, sizeof(char), strlen(to_write), fp);
-	fclose(fp);
+        FILE *fp = fopen(location, "w");
+        if (fp == NULL) {
+            perror("Failed to open file");
+            exit(-1);
+        }
+        
+        fwrite(to_write, sizeof(char), strlen(to_write), fp); // TODO: error handling
+        fclose(fp);
 
         //TODO(step5): free any arrays that are allocated using malloc!! Free the string returned from extract_root_directory()!! It is allocated using malloc in extract_root_directory()
-	free(root_dir);
+        free(root_dir);
 
     }else{
         //TODO(final submission): write the string to pipe
