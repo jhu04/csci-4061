ccd1fd793d118192ee48e78a8d86c7d3930c7db1
Sameen-R <123837744+SalmonRamen@users.noreply.github.com>
2023-12-02 23:50:30 -0600

    tried to implement directory traversal  for client

---
+++ project-4/README.md
@@ -0,0 +1,52 @@
+Project group number: 13
+
+Group member names and x500s:
+- Jeffrey Hu (hu000557)
+- Devajya Khanna (khann077)
+- Sameen Rahman (rahma262)
+
+The name of the CSELabs computer that you tested your code on: csel-kh1260-03.cselabs.umn.edu
+
+Any changes you made to the Makefile or existing files that would affect grading: N/A
+
+Plan outlining individual contributions for each member of your group:
+* We plan to meet on Zoom and have one person share their screen while the others assist by reviewing, looking up information in the writeup/documentation, etc.
+* Every 45 minutes, we will switch the screen sharer/typer. This should result in equal contributions for each member.
+
+Plan on how you are going to construct the client handling thread and how
+you plan to send the package according to the protocol
+
+Server Listening Thread:
+Opening a listening socket
+bind port # to it
+loop (listen --> accept connection and invoke client handling thread)
+
+
+__Client Handling Thread__:
+Create Connection Socket
+
+while True{
+Receive Packet
+
+recv a packet from client
+
+if received a IMG_OP_EXIT packet
+	break;
+//else received a IMG_OP_ROTATE packet --> image processing on it
+
+Image Processing
+
+//Sending packets: if error occurs, create an IMG_OP_NAK packet and send it to client
+//Sending packets: if successful, create an IMG_OP_ACK packet with processed image data and send to client
+if error occurs
+	send IMG_OP_NAK packet
+
+send IMG_OP_ACK packet
+}
+
+Close connection socket
+Clean-up
+
+//Main server thread doesn't need to intervene, this client handler just drops out
+pthread_detach() //release resources
+pthread_exit()
--- project-4/src/client.c
+++ project-4/src/client.c
@@ -28,12 +28,54 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Usage: ./client File_Path_to_images File_Path_to_output_dir Rotation_angle. \n");
         return 1;
     }
-    
+    char *image_directory = argv[1];
+    char *output_directory = argv[2];
+    int rotation_angle = atoi(argv[3]);
+
+    request_t queue[BUFFER_SIZE];
+    int queue_size = 0;
+
     // Set up socket
 
     // Connect the socket
 
     // Read the directory for all the images to rotate
+    DIR *directory = opendir(image_directory);
+    if(directory == NULL){
+        perror("Failed to open directory");
+        exit(1);
+    }
+    struct dirent *entry;
+
+    while((entry = readdir(directory)) != NULL){
+        char* entry_name = entry->d_name;
+        if(strcmp(entry_name, ".") == 0 || strcmp(entry_name, "..") == 0){
+            continue;
+        }
+        if(entry->d_type == DT_REG){
+            // TODO: make sure to free this (in worker after done processing)
+            char *path_buf = malloc(BUFFER_SIZE * sizeof(char));
+            memset(path_buf, '\0', BUFFER_SIZE * sizeof(char));
+            strcpy(path_buf, image_directory);
+            strcat(path_buf, "/");
+            strcat(path_buf, entry->d_name);
+
+            queue[queue_size] = (request_t) {.rotation_angle = rotation_angle, .file_name = path_buf};
+	    queue_size++;
+
+            // prints queue for debugging purposes
+            for (int i = 0; i < queue_size; ++i) {
+                printf("queue[%d].filename = %s\n", i, queue[i].file_name);
+            }
+            printf("\n");
+        }
+
+    }
+    if(closedir(directory) == -1){
+        perror("Failed to close directory");
+        exit(-1);
+    }
+
 
     // Send the image data to the server
 
